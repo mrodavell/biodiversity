18be5e5ce90f041695d045e4f5a31409f52c2bab
mrodavell <mail.jomar.llevado@gmail.com>
2025-03-25 06:58:06 +0800

    added analytics

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from "@vercel/analytics/react";
 
 
 export default function App() {
   return <>
+    <Analytics />
     <RouterProvider future={{ v7_startTransition: true }} router={router} />
     <ToastContainer />
   </>
